d5505563ab1920a1f13053ac8d315802d49e6910
Rohit Pandey <56378143+rohit-pandey96@users.noreply.github.com>
2020-08-23 02:55:56 +0530

    added profile upload

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -18,6 +18,7 @@ export class ProfileComponent implements OnInit {
   profileImageForm : FormGroup;
   fileDate : File = null;
   previewUrl : any = null;
+  profilePicture : String = null;
   constructor(private service : ProfileService, private fb : FormBuilder, private authService : AuthService, private spinner : NgxSpinnerService) { 
    this.username = this.authService.getUsername();
    this.emailid = this.authService.getUserEmail();
@@ -38,6 +39,7 @@ export class ProfileComponent implements OnInit {
       .then((res)=>{
         if(res.success){
             this.about = res.user.about;
+            this.profilePicture = res.user.profilePic;
             this.myPosts = this.myPosts.concat(res.user.posts);
         }
       })
@@ -63,7 +65,40 @@ export class ProfileComponent implements OnInit {
     reader.readAsDataURL(this.fileDate);
     reader.onload = (_event)=>{
       this.previewUrl = reader.result;
-      console.log(this.previewUrl);
+    }
+  }
+
+  uploadProfilePic(){
+    this.spinner.show();
+    this.profileImageForm.value.profilePic = this.previewUrl; 
+   // this.service.temp(this.authService.getUserId(), this.profileImageForm.value);
+    this.service.uploadProfile(this.authService.getUserId(), this.profileImageForm.value)
+    .then((res)=>{
+        if(res.success){
+          
+          this.profilePicture = res.user.profilePic;  
+          this.previewUrl = null;
+        }
+        
+    }).then(()=>{
+      console.log(this.profilePicture); 
+      this.spinner.hide();
+    })
+    
+  }
+  deleteProfilePic(){
+    this.spinner.show();
+    if(this.previewUrl !=null){
+      this.previewUrl = null;
+      this.spinner.hide();
+    }else{
+      this.service.deleteProfilePic(this.authService.getUserId())
+      .then((res)=>{
+        if(res.success){
+          this.profilePicture = null;
+        }
+        this.spinner.hide();
+      })
     }
   }
 
--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,17 +1,53 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-
+  fileUploadProgress = '0%';
   constructor(private http : HttpClient) { }
 
   getProfile(uid) : Promise<any>{
   
     return this.http.get(environment.userAPIUrl+'/getProfile/'+uid)
+    .toPromise()
+    .then((res)=>{
+      return res;
+    })
+
+  }
+
+  uploadProfile(uid, data): Promise<any>{
+    
+    return this.http.post(environment.userAPIUrl+'/uploadProfilePic/'+uid, data)
+    .toPromise()
+    .then((res)=>{
+      return res;
+    })
+
+  }
+
+  temp(uid, data){
+     this.http.post(environment.userAPIUrl+'/uploadProfilePic/'+uid, data, 
+    {reportProgress : true,
+    observe : 'events'})
+    .subscribe((events)=>{
+      if(events.type === HttpEventType.UploadProgress) {
+        this.fileUploadProgress = Math.round(events.loaded / events.total * 100) + '%';
+        console.log(this.fileUploadProgress);
+      } else if(events.type === HttpEventType.Response) {
+        this.fileUploadProgress = '';
+        console.log(events.body);          
+        alert('SUCCESS !!');
+      }
+    }) 
+  }
+
+  deleteProfilePic(uid): Promise<any>{
+  
+    return this.http.delete(environment.userAPIUrl+'/deleteProfilePic/'+uid)
     .toPromise()
     .then((res)=>{
       return res;
