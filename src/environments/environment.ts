7178c95744eb6974c62ad41103d5e4773585694a
Rohit Pandey <56378143+rohit-pandey96@users.noreply.github.com>
2020-11-16 16:50:02 +0530

    uncommented env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // pingServerUrl : 'http://localhost:4000/pingServer',
-  // userAPIUrl : 'http://localhost:4000/user'
+  pingServerUrl : 'http://localhost:4000/pingServer',
+  userAPIUrl : 'http://localhost:4000/user'
   // userAPIUrl : 'http://localhost:4000/user' 
 };
 
